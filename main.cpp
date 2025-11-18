ba557652ce57bd914ad0996910a0a2d10e9fcaa5
Sardor Urolov <sardoruralov2007@gmail.com>
2025-11-18 13:27:32 +0500

    Initial commit

--- main.cpp
+++ main.cpp
@@ -9,7 +9,12 @@ int main() {
         domain = "nslookup" + domain;
         system(domain.c_str());
     }*///Sayt IP manzilini aniqlash
-
-
+    int grade[] = {80, 70, 90, 85};
+    for (int i = 0; i < 4; i++) {
+        cout << grade[i] << " " << endl;
+    } cout << "Reverse" << endl;
+    for (int i = 3; i >= 0; --i) {
+        cout << grade[i] << " " << endl;
+    }
 
 }

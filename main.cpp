854a59008773c2029558b617b1dfca6b27b5e17e
Sardor Urolov <sardoruralov2007@gmail.com>
2025-11-18 10:36:52 +0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    /*string domain;
+    while (1) {
+        cout << "\nDomain name: ";
+        getline(cin, domain);
+        domain = "nslookup" + domain;
+        system(domain.c_str());
+    }*///Sayt IP manzilini aniqlash
+
+
+
+}

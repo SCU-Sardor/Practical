4cc451565da8ef37984e4f8a1ba34b984682dcde
Sardor Urolov <sardoruralov2007@gmail.com>
2025-12-05 13:56:19 +0500

    Initial commit

--- pointers.cpp
+++ pointers.cpp
@@ -1,18 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    /*int ball = 5, ball2 = 4;
+/*int main() {
+    int ball = 5, ball2 = 4;
     int *pointer = &ball;
     cout << pointer <<endl;
     pointer = &ball2;
     cout << pointer <<endl;//o'zgaruvchining xotira manzili
-    cout << *pointer <<endl;//15*///pointers haqida tushuncha
+    cout << *pointer <<endl;//pointers haqida tushuncha
     int x = 5;
     int *p = &x;
     x = x + 4;
     x = *p + 4;
     *p = *p +4;
     cout << *p << endl;
+}*///Pointerlar haqida tuhsuncha
 
+/*void function(int *var) {
+    *var = 29;
 }
+
+int main() {
+    int x = 11;
+    function(&x);
+    cout << x << endl;
+}*///Functions and Pointers
